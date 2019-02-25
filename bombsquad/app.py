5ba23293f5f9bccba5e110b1a629a372b4f7e0bf
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-02-25 14:20:19 +0100

    homepage/start button

--- bombsquad/app.py
+++ bombsquad/app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/home')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def home():
     return render_template('homepage.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        debug=True,
+        port=5000
+    )
