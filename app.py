fca1d76d1029d753d1de72d56d2b667f6bfb416b
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-02-26 15:49:42 +0100

    game page layout and dragula

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def home():
     return render_template('homepage.html')
 
 
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
 if __name__ == '__main__':
     app.run(
         debug=True,
