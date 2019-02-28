3b3af3b82f50b38c396df717b198ba86b7b6522e
kincsojerne <voros.kincsojerne@gmail.com>
2019-02-28 13:18:09 +0100

    task load from JSON version 1

--- static/data.js
+++ static/data.js
@@ -1,9 +1,20 @@
-var snippets = {"id1" : "num_list = [1, 2, 3, 4]\n" + "new_number = 0",
+var snippets = [{"id1" : "num_list = [1, 2, 3, 4]\n" + "new_number = 0",
         "id2" : "for i in range(len(num_list)):" + "\n" + "new_number += 1",
         "id3" : "num_list.append(new_number)",
-        "id4" : "return num_list"};
+        "id4" : "return num_list"},
+        {"id1" : "első", "id2" : "második", "id3" : "harmadik", "id4" : "negyedik"}];
 
-document.getElementById("l1").innerHTML = snippets.id1;
-document.getElementById("l2").innerHTML = snippets.id2;
-document.getElementById("l3").innerHTML = snippets.id3;
-document.getElementById("l4").innerHTML = snippets.id4;
+function loadTask(i) {
+        document.getElementById("l1").innerHTML = snippets[i].id1;
+        document.getElementById("l2").innerHTML = snippets[i].id2;
+        document.getElementById("l3").innerHTML = snippets[i].id3;
+        document.getElementById("l4").innerHTML = snippets[i].id4;
+        i ++;
+        return i;
+}
+
+function main() {
+        loadTask(0);
+}
+
+main();
